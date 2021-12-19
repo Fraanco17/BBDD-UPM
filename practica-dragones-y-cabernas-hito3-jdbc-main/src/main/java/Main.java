a0cd86ee3e3f396bd4e83f92db5c2a1e3fe1b3e5
ErYavi <96310629+ErYavi@users.noreply.github.com>
2021-12-19 12:00:17 +0100

    Maun update

--- practica-dragones-y-cabernas-hito3-jdbc-main/src/main/java/Main.java
+++ practica-dragones-y-cabernas-hito3-jdbc-main/src/main/java/Main.java
@@ -2,23 +2,22 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import game.model.Dragon;
+import game.model.Hacha;
 
 public class Main {
     // @TODO: Sustituya xxxx por los parámetros de su conexión
 
-    private static final String DB_SERVER = "3306";
+    private static final String DB_SERVER = "127.0.0.1";
 
     private static final int DB_PORT = 3306;
 
-    private static final String DB_NAME = "3306";
+    private static final String DB_NAME = "mysql";
 
-    private static final String DB_USER = "3306";
+    private static final String DB_USER = "root";
 
-    private static final String DB_PASS = "3306";
+    private static final String DB_PASS = "1705";
 
     private static Connection conn;
-
-
 
     public static void main(String[] args) throws Exception {
 
@@ -29,34 +28,40 @@ public class Main {
 
         // @TODO pruebe sus funciones
 
-
-
         conn.close();
     }
 
     // @TODO resuelva las siguientes funciones...
 
-    public static void nuevo_dragon(String nombre){
-        // @TODO: complete este método para que cree un nuevo dragón en la base de datos
+    public static void nuevo_dragon(String nombre) {
 
     }
 
-    public static List<Dragon> squad_derrota_dragones(Long id_squad){
-        // @TODO: complete este método para que devuelva una lista de los dragones derrotados por el squad
-        // Tenga en cuenta que la consulta a la base de datos le devolverá un ResultSet sobre el que deberá
-        // ir iterando y creando un objeto dragon para cada uno de los dragones, y añadirlos a la lista
+    public static List<Dragon> squad_derrota_dragones(Long id_squad) {
+        ArrayList<Dragon> lista = new ArrayList<Dragon>();
+        // @TODO: complete este método para que devuelva una lista de los dragones
+        // derrotados por el squad
+        // Tenga en cuenta que la consulta a la base de datos le devolverá un ResultSet
+        // sobre el que deberá
+        // ir iterando y creando un objeto dragon para cada uno de los dragones, y
+        // añadirlos a la lista
         return lista;
     }
 
-    public static List<Hacha> mostrar_hachas(String nombre_forja){
-        // @TODO: complete este método para que muestre por pantalla las hachas que pueden forjarse en "nombre_forja"
-        // Tenga en cuenta que la consulta a la base de datos le devolverá un ResultSet sobre el que deberá
-        // ir iterando y creando un objeto con cada hacha disponible en esa forja, y añadirlos a la lista
+    public static List<Hacha> mostrar_hachas(String nombre_forja) {
+        ArrayList<Hacha> lista = new ArrayList<>();
+        // @TODO: complete este método para que muestre por pantalla las hachas que
+        // pueden forjarse en "nombre_forja"
+        // Tenga en cuenta que la consulta a la base de datos le devolverá un ResultSet
+        // sobre el que deberá
+        // ir iterando y creando un objeto con cada hacha disponible en esa forja, y
+        // añadirlos a la lista
         return lista;
     }
 
-    public static String espada_porta_guerrero(String nombre_guerrero){
-        // @TODO: complete este método para que devuelva el nombre de la espada que porta el guerrero "nombre_guerrero"
+    public static String espada_porta_guerrero(String nombre_guerrero) {
+        // @TODO: complete este método para que devuelva el nombre de la espada que
+        // porta el guerrero "nombre_guerrero"
         return "espadón";
     }
 
