a6675f0df613fad5206f73dfc785bc1630dc5ffb
Victor <coundbla@gmail.com>
2021-12-20 12:11:36 +0100

    daga

--- hibernate-practica-dragones-y-cabernas-hito3-main/src/main/java/ormRPGgame/model/Daga.java
+++ hibernate-practica-dragones-y-cabernas-hito3-main/src/main/java/ormRPGgame/model/Daga.java
@@ -5,8 +5,19 @@ import java.util.HashSet;
 import java.util.Set;
 
 // @TODO completar las anotaciones de la clase
+@Entity
+@Table(name = "daga")
 public class Daga {
+    @Id
+    @Column(name = "nombre_daga")
     private String name;
+
+    @Column(name = "daño")
+    private int daño;
+
+    @Column(name = "peso")
+    private float peso;
+
     // @TODO completar las anotaciones de todos los atributos
 
     public Daga(String nombre){
@@ -15,4 +26,22 @@ public class Daga {
     }
 
     public String getnombre(){ return this.name;}
+
+    public int getDaño() {
+        return daño;
+    }
+
+    public void setDaño(int daño) {
+        this.daño = daño;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+
 }
